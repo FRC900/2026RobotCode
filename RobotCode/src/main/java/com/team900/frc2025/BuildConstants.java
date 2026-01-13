0e033996d7c8b795fc0df3e9c8f4192a67e58ddd
Vidhatu Patel <62958219+Kemchobro@users.noreply.github.com>
2026-01-13 03:11:40 -0500

    fixed vendor deps

---
+++ RobotCode/src/main/java/com/team900/frc2025/BuildConstants.java
@@ -0,0 +1,19 @@
+package com.team900.frc2025;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "RobotCode";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "0b6cc4869ca5236e02fd1250fcaed411d753d779";
+  public static final String GIT_DATE = "2026-01-12 18:18:41 PST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-13 00:08:28 PST";
+  public static final long BUILD_UNIX_TIME = 1768291708603L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
