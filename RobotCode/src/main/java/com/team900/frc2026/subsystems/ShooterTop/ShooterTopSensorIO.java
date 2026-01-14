ee016bd6abab57fa864b30d467820a602b073f3f
jrfranklin2009 <jrfranklin2009@gmail.com>
2026-01-14 17:13:18 -0500

    Creating code for the top and bottom shooters.

---
+++ RobotCode/src/main/java/com/team900/frc2026/subsystems/ShooterTop/ShooterTopSensorIO.java
@@ -0,0 +1,17 @@
+package com.team900.frc2026.subsystems.ShooterTop;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public class ShooterTopSensorIO {
+
+    @AutoLog
+
+    public class ShooterTopSensorInputs {
+
+        public boolean ExampleBoolean;
+
+    }
+
+     public default void readInputs(ShooterTopSensorIO.ShooterTopSensorInputs inputs) {}
+
+}
