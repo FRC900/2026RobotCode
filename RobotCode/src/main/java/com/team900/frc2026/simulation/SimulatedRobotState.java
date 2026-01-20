ce4ac5ba8942da9d615b336d02f49f40feeded5f
jrfranklin2009 <jrfranklin2009@gmail.com>
2026-01-19 22:44:53 -0500

    some sim skeleton code

--- RobotCode/src/main/java/com/team900/frc2026/simulation/SimulatedRobotState.java
+++ RobotCode/src/main/java/com/team900/frc2026/simulation/SimulatedRobotState.java
@@ -1,5 +1,7 @@
 package com.team900.frc2026.simulation;
 
+import com.team254.lib.time.RobotTime;
+import com.team900.frc2026.RobotContainer;
 import com.team900.frc2026.RobotState;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -10,7 +12,45 @@ import edu.wpi.first.math.interpolation.TimeInterpolatableBuffer;
 
 public class SimulatedRobotState {
 
-        TimeInterpolatableBuffer<Pose2d> fieldToRobotSimulatedTruth =
-            TimeInterpolatableBuffer.createBuffer(RobotState.LOOKBACK_TIME);
+    public enum BallState {
+        STATE_ONE,
+        STATE_TWO
+    }
+
+    TimeInterpolatableBuffer<Pose2d> fieldToRobotSimulatedTruth =
+        TimeInterpolatableBuffer.createBuffer(RobotState.LOOKBACK_TIME);
+
+    private BallState ballState = BallState.STATE_ONE;
+
+    private RobotContainer container;
+
+    public SimulatedRobotState(RobotContainer container){
+        this.container = container;
+    }
+
+    synchronized public void addFieldToRobot(Pose2d pose){
+        fieldToRobotSimulatedTruth.addSample(RobotTime.getTimestampSeconds(), pose);
+    }
+
+    synchronized public BallState getBallState() {
+        return this.ballState;
+    }
+
+    synchronized public void setBallState(BallState state) {
+        this.ballState = state;
+    }
+
+    synchronized public Pose2d getLatestFieldToRobot() {
+        var entry = fieldToRobotSimulatedTruth.getInternalBuffer().lastEntry();
+        if (entry == null) {
+        return null;
+        }
+        return entry.getValue();
+    }
+
+    synchronized public void updateSim() {
+        
+    }
+
 
 }
