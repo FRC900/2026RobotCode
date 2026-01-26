0654e9a3761f1390e5fa0c21887697bbf9a8d621
anshmenghani <ansh.menghani@gmail.com>
2026-01-26 17:26:44 -0500

    functionalized (is that a word) code

--- RobotCode/src/main/ProjectileMotionSim/FuelPathRK4.py
+++ RobotCode/src/main/ProjectileMotionSim/FuelPathRK4.py
@@ -1,28 +1,16 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 # Define Constants 
-ball_radius = 0.150114
-ball_farea = np.pi * (ball_radius**2)
+ball_radius = 0.0762
+ball_csarea = np.pi * (ball_radius**2)
 ball_mass = 0.21500278
-g = 9.81
-t0 = 0
-vx0 = 10
-vy0 = 7
-vz0 = 5
-sx0 = 0
-sy0 = 0
-sz0 = 0
-v0_vec = np.array([vx0, vy0, vz0], dtype=float)
-p0_vec = np.array([sx0, sy0, sz0], dtype=float)
-omega0 = 12 * np.pi
+omega0 = 0
 I = 0.4 * ball_mass * (ball_radius**2)
 n_hat = 1
 rho = 1.195
 mu = 1.835e-5
-dt = 0.01
-sim_end_time = 10
+g = 9.81
 
 
 class RungeKutta4:
@@ -57,7 +45,7 @@ def reynolds(speed):
     return (rho*speed*2*ball_radius) / mu
 
 def spin_coeff(Re): # complete
-    return 500
+    return 0.02
 
 def omega(t, speed):
     if omega0 < 1e-8:
@@ -78,16 +66,16 @@ def lift_coeff(S): # complete
     return 1.5 * S
     
 def dvxdt(t, speed, vx, vy):
-    coeff = (-1*rho*ball_farea*speed) / (2*ball_mass)
+    coeff = (-1*rho*ball_csarea*speed) / (2*ball_mass)
     return coeff * ((drag_coeff(reynolds(speed))*vx) + (lift_coeff(shear(t, speed))*vy))
 
 def dvydt(t, speed, vx, vy):
-    coeff = (rho*ball_farea*speed) / (2*ball_mass)
+    coeff = (rho*ball_csarea*speed) / (2*ball_mass)
     return coeff * ((-1*drag_coeff(reynolds(speed))*vy) + (lift_coeff(shear(t, speed))*vx)) - g 
 
 def dvzdt(speed, vz):
     coeff = (-1*rho) / (2*ball_mass)
-    return coeff * drag_coeff(reynolds(speed)) * ball_farea * speed * vz
+    return coeff * drag_coeff(reynolds(speed)) * ball_csarea * speed * vz
 
 def dvdt(t, a):
     vx, vy, vz = a
@@ -99,98 +87,98 @@ def dvdt(t, a):
 
     return np.array([dvx, dvy, dvz], dtype=float)
 
-vel_approx = RungeKutta4(dvdt, t0, dt, v0_vec)
-vel_approx.sim(sim_end_time)
-v_list = vel_approx.a_list
-vx_list, vy_list, vz_list = map(list, zip(*vel_approx.a_list))
+def solve(vx0, vy0, vz0, omega0_param, t0=0, dt=0.01, sim_end_time=10, sx0=0, sy0=0, sz0=0):
+    global omega0
+    omega0 = omega0_param
+
+    v0_vec = np.array([vx0, vy0, vz0], dtype=float)
+    p0_vec = np.array([sx0, sy0, sz0], dtype=float)
+
+    vel_approx = RungeKutta4(dvdt, t0, dt, v0_vec)
+    vel_approx.sim(sim_end_time)
+    v_list = vel_approx.a_list
+    vx_list, vy_list, vz_list = map(list, zip(*vel_approx.a_list))
 
 
-def dsdt(t, _):
-    idx = int(t / dt)
+    def dsdt(t, _):
+        idx = int(t / dt)
 
-    dsx = v_list[idx][0]
-    dsy = v_list[idx][1]
-    dsz = v_list[idx][2]
+        dsx = v_list[idx][0]
+        dsy = v_list[idx][1]
+        dsz = v_list[idx][2]
 
-    return np.array([dsx, dsy, dsz], dtype=float)
+        return np.array([dsx, dsy, dsz], dtype=float)
 
-pos_approx = RungeKutta4(dsdt, t0, dt, p0_vec)
-pos_approx.sim(sim_end_time)
-sx_list, sy_list, sz_list = map(list, zip(*pos_approx.a_list))
+    pos_approx = RungeKutta4(dsdt, t0, dt, p0_vec)
+    pos_approx.sim(sim_end_time)
+    sx_list, sy_list, sz_list = map(list, zip(*pos_approx.a_list))
 
-sy_list = [i for i in sy_list if i >= 0]
-vel_approx_tlist, pos_approx_tlist = vel_approx.t_list.copy()[:len(sy_list)], pos_approx.t_list.copy()[:len(sy_list)]
-sx_list, sz_list, vx_list, vy_list, vz_list = sx_list[:len(sy_list)], sz_list[:len(sy_list)], vx_list[:len(sy_list)], vy_list[:len(sy_list)], vz_list[:len(sy_list)]
+    new_sy_list = []
+    for i in sy_list:
+        if i < 0:
+            break
+        new_sy_list.append(i)
+    sy_list = new_sy_list
+    vel_approx_tlist, pos_approx_tlist = vel_approx.t_list.copy()[:len(sy_list)], pos_approx.t_list.copy()[:len(sy_list)]
+    sx_list, sz_list, vx_list, vy_list, vz_list = sx_list[:len(sy_list)], sz_list[:len(sy_list)], vx_list[:len(sy_list)], vy_list[:len(sy_list)], vz_list[:len(sy_list)]
+    
+    return vel_approx_tlist, vx_list, vy_list, vz_list, pos_approx_tlist, sx_list, sy_list, sz_list
+
+def plot_solutions(vel_approx_tlist, vx_list, vy_list, vz_list, pos_approx_tlist, sx_list, sy_list, sz_list):
+    plt.scatter(vel_approx_tlist, vx_list, s=1, color="red")
+    plt.xlabel("Time (s)")
+    plt.ylabel("X-Velocity (m/s)")
+    plt.title("X-Velocity vs. Time")
+    plt.show()
+
+    plt.scatter(vel_approx_tlist, vy_list, s=1, color="blue")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Y-Velocity (m/s)")
+    plt.title("Y-Velocity vs. Time")
+    plt.show()
+
+    plt.scatter(vel_approx_tlist, vz_list, s=1, color="green")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Z-Velocity (m/s)")
+    plt.title("Z-Velocity vs. Time")
+    plt.show()
+
+    plt.scatter(pos_approx_tlist, sx_list, s=1, color="red")
+    plt.xlabel("Time (s)")
+    plt.ylabel("X-Position (m)")
+    plt.title("X-Position vs. Time")
+    plt.show()
+
+    plt.scatter(pos_approx_tlist, sy_list, s=1, color="blue")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Y-Position (m)")
+    plt.title("Y-Position vs. Time")
+    plt.show()
+
+    plt.scatter(pos_approx_tlist, sz_list, s=1, color="green")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Z-Position (m)")
+    plt.title("Z-Position vs. Time")
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    t_f = pos_approx_tlist
+    scf = ax.scatter(
+        sx_list, sy_list, sz_list,
+        c=t_f, s=6, cmap='cividis'
+    )
+    plt.colorbar(scf, ax=ax, label='Time--Full Model (s)')
+    ax.plot(sx_list, sy_list, sz_list, linewidth=1, label="Full Trajectory", color="olive")
+    ax.set_xlabel('X Position (m)')
+    ax.set_ylabel('Y Position (m)')
+    ax.set_zlabel('Z Position (m)')
+    ax.set_title('3D Trajectory with Time')
+    ax.legend()
+
+    plt.show()
 
 
-gy_list = [(vy0*i*dt-(0.5*g*((i*dt)**2))) for i in range(int(2*vy0/(g*dt))+1)]
-gx_list = [vx0*i*dt for i in range(len(gy_list))]
-gz_list = [vz0*i*dt for i in range(len(gy_list))]
-
-
-plt.scatter(vel_approx_tlist, vx_list, s=1, color="red")
-plt.xlabel("Time (s)")
-plt.ylabel("X-Velocity (m/s)")
-plt.title("X-Velocity vs. Time")
-plt.show()
-
-plt.scatter(vel_approx_tlist, vy_list, s=1, color="blue")
-plt.xlabel("Time (s)")
-plt.ylabel("Y-Velocity (m/s)")
-plt.title("Y-Velocity vs. Time")
-plt.show()
-
-plt.scatter(vel_approx_tlist, vz_list, s=1, color="green")
-plt.xlabel("Time (s)")
-plt.ylabel("Z-Velocity (m/s)")
-plt.title("Z-Velocity vs. Time")
-plt.show()
-
-plt.scatter(pos_approx_tlist, sx_list, s=1, color="red")
-plt.xlabel("Time (s)")
-plt.ylabel("X-Position (m)")
-plt.title("X-Position vs. Time")
-plt.show()
-
-plt.scatter(pos_approx_tlist, sy_list, s=1, color="blue")
-plt.xlabel("Time (s)")
-plt.ylabel("Y-Position (m)")
-plt.title("Y-Position vs. Time")
-plt.show()
-
-plt.scatter(pos_approx_tlist, sz_list, s=1, color="green")
-plt.xlabel("Time (s)")
-plt.ylabel("Z-Position (m)")
-plt.title("Z-Position vs. Time")
-plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-t_f = pos_approx_tlist
-scf = ax.scatter(
-    sx_list, sy_list, sz_list,
-    c=t_f, s=6, cmap='cividis'
-)
-plt.colorbar(scf, ax=ax, label='Time--Full Model (s)')
-ax.plot(sx_list, sy_list, sz_list, linewidth=1, label="Full Trajectory", color="olive")
-
-t_g = [i*dt for i in range(len(gx_list))]
-scg = ax.scatter(
-    gx_list, gy_list, gz_list,
-    c=t_g, s=6, cmap='inferno'
-)
-plt.colorbar(scg, ax=ax, label='Time--Gravity Model (s)')
-ax.plot(gx_list, gy_list, gz_list, linewidth=1, label="Gravity Only", color="blue")
-
-ax.set_xlabel('X Position (m)')
-ax.set_ylabel('Y Position (m)')
-ax.set_zlabel('Z Position (m)')
-ax.set_title('3D Trajectory with Time')
-ax.legend()
-
-v1 = np.array([gx_list[-1], gy_list[-1], gz_list[-1]])
-v2 = np.array([sx_list[-1], sy_list[-1], sz_list[-1]])
-print(np.linalg.norm(v2-v1))
-
-plt.show()
+vel_approx_tlist, vx_list, vy_list, vz_list, pos_approx_tlist, sx_list, sy_list, sz_list = solve(8, 12, 3, 500)
+plot_solutions(vel_approx_tlist, vx_list, vy_list, vz_list, pos_approx_tlist, sx_list, sy_list, sz_list)
