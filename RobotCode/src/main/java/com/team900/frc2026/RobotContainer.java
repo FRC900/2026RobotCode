84029ee92421fa1c0e08b803b27a4b04ff55b3c0
anshmenghani <ansh.menghani@gmail.com>
2026-02-15 23:26:32 -0500

    Added commands and sim

--- RobotCode/src/main/java/com/team900/frc2026/RobotContainer.java
+++ RobotCode/src/main/java/com/team900/frc2026/RobotContainer.java
@@ -5,14 +5,23 @@
 package com.team900.frc2026;
 
 import com.team900.frc2026.subsystems.Turret.TurretIOHardware;
+import com.team900.frc2026.subsystems.Turret.TurretIOSim;
 import com.team900.frc2026.subsystems.Turret.TurretSubsystem;
 
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
-  private final TurretSubsystem Turret = new TurretSubsystem(new TurretIOHardware()); 
+  private final TurretSubsystem Turret;
+
   public RobotContainer() {
+    if (RobotBase.isReal()) {
+      Turret = new TurretSubsystem(new TurretIOHardware());
+    } else {
+      Turret = new TurretSubsystem(new TurretIOSim());
+    }
+
     configureBindings();
   }
 
