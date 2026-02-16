aeac58595a612ec7075b0c33cf962ce2eda5eaf4
anshmenghani <ansh.menghani@gmail.com>
2026-02-15 23:40:14 -0500

    Integrated sim and deg

--- RobotCode/src/main/java/com/team900/frc2026/subsystems/Hood/HoodSubsystem.java
+++ RobotCode/src/main/java/com/team900/frc2026/subsystems/Hood/HoodSubsystem.java
@@ -1,15 +1,12 @@
 package com.team900.frc2026.subsystems.Hood;
 
-import com.team254.lib.subsystems.MotorIO;
-import com.team254.lib.subsystems.MotorInputsAutoLogged;
-import com.team254.lib.subsystems.ServoMotorSubsystem;
 import com.team254.lib.time.RobotTime;
 
 import com.team900.frc2026.Constants;
 
 import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -18,44 +15,67 @@ import java.util.function.Supplier;
 
 public class HoodSubsystem extends SubsystemBase {
     private final HoodInputsAutoLogged inputs = new HoodInputsAutoLogged();
-
     private final HoodIO io;
 
-    private double hoodSetpointRadiansFromCenter = 0.0;
+    private double positionSetpointRad = 0.0;
+    private double velocitySetpointRadPerSec = 0.0;
 
     public HoodSubsystem(final HoodIO io) {
         this.io = io;
     } 
 
-    public void setTeleopDefaultCommand() {
-        this.setDefaultCommand(run(() -> {
-            setPositionSetpointImpl(hoodSetpointRadiansFromCenter, 0.0);
-        }).withName("Hood Maintain Setpoint (default)"));
-    }
-
     @Override
     public void periodic() {
         double timestamp = RobotTime.getTimestampSeconds();
+
         io.readInputs(inputs);
         Logger.processInputs("Hood", inputs);
         io.update(inputs);
+    
+        double clamped =
+            Math.max(Constants.HoodConstants.kHoodMinPositionRadians,
+            Math.min(Constants.HoodConstants.kHoodMaxPositionRadians, positionSetpointRad));
+        
+        positionSetpointRad = clamped;
+        io.setPositionSetpoint(positionSetpointRad, velocitySetpointRadPerSec);
 
         Logger.recordOutput("Hood/positionRad", inputs.positionRad);
         Logger.recordOutput("Hood/latencyPeriodicSec", RobotTime.getTimestampSeconds() - timestamp);
     }
 
-    private void setPositionSetpointImpl(double radiansFromCenter, double radsPerSec) {
-        io.setPositionSetpoint(radiansFromCenter, radsPerSec);
-        hoodSetpointRadiansFromCenter = radiansFromCenter;
+    public Command angleCommand(Supplier<Double> angleRadSupplier) {
+    return run(() -> positionSetpointRad = angleRadSupplier.get())
+            .withName("Hood Track Setpoint");
     }
 
-    public Command angleCommand(Supplier<Double> angleRadSupplier) {
-    return run(() -> setPositionSetpointImpl(angleRadSupplier.get(), 0.0))
-            .withName("Hood Track Setpoint");
+    public Command angleDegreesCommand(Supplier<Double> angleDegSupplier) {
+        return run(() ->
+            positionSetpointRad = Units.degreesToRadians(angleDegSupplier.get())
+        ).withName("Hood Track Setpoint (deg)");
     }
 
     public void resetZero() {
         io.resetZeroPoint();
-        hoodSetpointRadiansFromCenter = 0.0;
+        positionSetpointRad = 0.0;
+    }
+
+    public boolean atSetpoint() {
+        return Math.abs(inputs.positionRad - positionSetpointRad) < Constants.HoodConstants.kHoodPositionTolerance;
+    }
+
+    public double getPositionRadians() {
+        return inputs.positionRad;
+    }
+
+    public double getPositionDegrees() {
+        return Math.toDegrees(inputs.positionRad);
+    }
+
+    public double getSetpointRadians() {
+        return positionSetpointRad;
+    }
+
+    public double getSetpointDegrees() {
+        return Math.toDegrees(positionSetpointRad);
     }
 }
