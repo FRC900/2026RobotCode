bb3cc60d3b9d1d02288330841fc4255314ea3759
Rex Chen <rexchen0125@gmail.com>
2026-02-22 01:52:19 -0500

    gradle stuff

---
+++ src/main/java/com/team900/frc2026/BuildConstants.java
@@ -0,0 +1,19 @@
+package com.team900.frc2026;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026RobotCode";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 53;
+  public static final String GIT_SHA = "b7bd52808e6b9b1503d5b1ee4f2c3393a62bd768";
+  public static final String GIT_DATE = "2026-02-21 19:42:39 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-22 00:35:35 EST";
+  public static final long BUILD_UNIX_TIME = 1771738535832L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
