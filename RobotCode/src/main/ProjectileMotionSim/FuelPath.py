b2f396a95615473bd11ca0f9e36a4681ab71bbd6
anshmenghani <ansh.menghani@gmail.com>
2026-02-23 14:52:02 -0500

    made it look cooler

--- RobotCode/src/main/ProjectileMotionSim/FuelPath.py
+++ RobotCode/src/main/ProjectileMotionSim/FuelPath.py
@@ -1,14 +1,22 @@
-# example usage of ProjectilePath.py
+# example usage of ProjectilePath.py for FRC 2026 Fuel
 
 import ProjectilePath as pp
 import FuelClearance as fc
-from numpy import rad2deg
+from numpy import array, rad2deg, linalg
+from tabulate import tabulate
 import time 
 
 # define targets and initial condition guesses
 xt = 10
 yt = 2
 zt = 10
+
+# define robot's initial velocities and Fuel initial spin
+vx_robot = 3
+vy_robot = 3
+vz_robot = 3
+v_mag_robot = linalg.norm(array([vx_robot, vy_robot, vz_robot]))
+omega_robot = 10
 
 # define initial solution guesses
 vxi0 = 10
@@ -18,16 +26,34 @@ omegai0 = 10
 
 # define fuel object and fuel_solver object
 fuel = pp.Projectile(0.0762, 0.226796)
-fuel_solver = pp.ProjectileSolver(fuel, xt, yt, zt, vxi0, vyi0, vzi0, omegai0, fix_speed=True, fix_omega=True, lm_iters=10, sim_end_time=5, dt=0.01, clearance_func=fc.hub_clearance)
+fuel_solver = pp.ProjectileSolver(fuel, xt, yt, zt, vxi0, vyi0, vzi0, omegai0, vx_frame=vx_robot, vy_frame=vy_robot, vz_frame=vz_robot, omega_frame=omega_robot, fix_speed=False, fix_omega=False, lm_iters=3, sim_end_time=5, dt=0.01, clearance_func=fc.hub_clearance)
 
 # solve for valid inputs and time the solver
 start = time.perf_counter()
 fuel_solver.levenberg_marquardt()
 end = time.perf_counter()
-print("\nLevenberg-Marquardt Time: ", 1000 * (end - start), "ms")
 
 # display results 
-vel_approx_tlist, vx_list, vy_list, vz_list, speedf, pos_approx_tlist, sx_list, sy_list, sz_list, omegaf = fuel.trajectory(fuel_solver.vx, fuel_solver.vy, fuel_solver.vz, fuel_solver.omega, dt=0.001, stop_on_y=yt) # smaller dt=more refined approximation
-print("\nInitial Speed:", fuel_solver.v_mag, "\nInitial X-Velocity:", fuel_solver.vx, "m/s", "\nInitial Y-Velocity:", fuel_solver.vy, "m/s", "\nInitial Z-Velocity:", fuel_solver.vz, "m/s", "\nInitial Spin (+, backspin | -, topspin):", fuel_solver.omega, "rad/s", "\nTheta:", rad2deg(fuel_solver.theta), "deg", "\nPhi:", rad2deg(fuel_solver.phi), "deg")
-print("\nFinal X-Position:", sx_list[-1], "m", "\nFinal Y-Position:", sy_list[-1], "m", "\nFinal Z-Position:", sz_list[-1], "m", "\n")
+vel_approx_tlist, vx_list, vy_list, vz_list, speedf, pos_approx_tlist, sx_list, sy_list, sz_list, omegaf = fuel.trajectory(fuel_solver.vx+vx_robot, fuel_solver.vy+vy_robot, fuel_solver.vz+vz_robot, fuel_solver.omega+omega_robot, dt=0.001, stop_on_y=yt) # smaller dt=more refined approximation
+
+table = [
+    ["LM Time (ms)", 1000 * (end - start)],
+    ["Shot Speed (m/s)", fuel_solver.v_mag],
+    ["Shot vx (m/s)", fuel_solver.vx],
+    ["Shot vy (m/s)", fuel_solver.vy],
+    ["Shot vz (m/s)", fuel_solver.vz],
+    ["Shot Spin (rad/s)", fuel_solver.omega],
+    ["Total Speed (m/s)", fuel_solver.v_mag + v_mag_robot],
+    ["Total vx (m/s)", fuel_solver.vx + vx_robot],
+    ["Total vy (m/s)", fuel_solver.vy + vy_robot],
+    ["Total vz (m/s)", fuel_solver.vz + vz_robot],
+    ["Total Spin (rad/s)", fuel_solver.omega + omega_robot],
+    ["Theta (deg)", rad2deg(fuel_solver.theta)],
+    ["Phi (deg)", rad2deg(fuel_solver.phi)],
+    ["Final X (m)", sx_list[-1]],
+    ["Final Y (m)", sy_list[-1]],
+    ["Final Z (m)", sz_list[-1]],
+]
+
+print(tabulate(table, headers=["Parameter", "Value"], tablefmt="rounded_grid"))
 fuel.plot_solutions(vel_approx_tlist, vx_list, vy_list, vz_list, pos_approx_tlist, sx_list, sy_list, sz_list)
