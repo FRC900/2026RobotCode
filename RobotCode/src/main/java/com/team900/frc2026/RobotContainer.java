f512d666acf883198e8e098f9a2590fd527bc8b7
ajain189 <aarushjain189@gmail.com>
2026-02-24 15:04:22 -0500

    wired up swerve drive with field-centric controls and gyro reset

--- RobotCode/src/main/java/com/team900/frc2026/RobotContainer.java
+++ RobotCode/src/main/java/com/team900/frc2026/RobotContainer.java
@@ -1,20 +1,76 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package com.team900.frc2026;
 
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.team900.frc2026.subsystems.drive.DriveIOHardware;
+import com.team900.frc2026.subsystems.drive.DriveSubsystem;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
+    // Subsystems
+    private final DriveSubsystem drive;
 
-  private void configureBindings() {}
+    // Controllers
+    private final CommandXboxController driverController =
+            new CommandXboxController(Constants.kDriverControllerPort);
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+    // Swerve requests
+    private final SwerveRequest.FieldCentric fieldCentricDrive =
+            new SwerveRequest.FieldCentric()
+                    .withDriveRequestType(DriveRequestType.OpenLoopVoltage)
+                    .withSteerRequestType(SteerRequestType.MotionMagicExpo)
+                    .withDeadband(Constants.DriveConstants.kDriveMaxSpeed * Constants.kJoystickDeadband)
+                    .withRotationalDeadband(
+                            Constants.DriveConstants.kDriveMaxAngularRate
+                                    * Constants.kJoystickDeadband);
+
+    private final SwerveRequest.SwerveDriveBrake brakeRequest = new SwerveRequest.SwerveDriveBrake();
+
+    private final SwerveRequest.PointWheelsAt pointRequest = new SwerveRequest.PointWheelsAt();
+
+    public RobotContainer() {
+        drive = buildDriveSubsystem();
+        configureDefaultCommands();
+        configureBindings();
+    }
+
+    private DriveSubsystem buildDriveSubsystem() {
+        return new DriveSubsystem(
+                new DriveIOHardware(
+                        Constants.DriveConstants.kDrivetrain.getDriveTrainConstants(),
+                        Constants.DriveConstants.kDrivetrain.getModuleConstants()));
+    }
+
+    private void configureDefaultCommands() {
+        // Field-centric: left stick - translation, right stick X - rotation
+        drive.setDefaultCommand(drive.applyRequest(() -> fieldCentricDrive
+                                        .withVelocityX(-driverController.getLeftY() * Constants.DriveConstants.kDriveMaxSpeed)
+                                        .withVelocityY(-driverController.getLeftX() * Constants.DriveConstants.kDriveMaxSpeed)
+                                        .withRotationalRate(-driverController.getRightX() * Constants.DriveConstants.kDriveMaxAngularRate)));
+    }
+
+    private void configureBindings() {
+        // // A button: brake (X-pattern)
+        // driverController.a().whileTrue(drive.applyRequest(() -> brakeRequest));
+
+        // // B button: point wheels at current right stick direction
+        // driverController.b().whileTrue(drive.applyRequest(() -> pointRequest.withModuleDirection(
+        //         new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))));
+
+        // Start button: reset gyro heading (zero the field-centric forward direction)
+        driverController.start().onTrue(Commands.runOnce(
+                                        () ->drive.resetOdometry(
+                                                new edu.wpi.first.math.geometry.Pose2d(
+                                                        drive.getPose().getTranslation(),
+                                                        new Rotation2d())))
+                                                .ignoringDisable(true));
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+    }
 }
