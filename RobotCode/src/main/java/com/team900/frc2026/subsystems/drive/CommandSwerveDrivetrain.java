bba6881392e4fb2b1bfeb2b1e2a0c80dbe0ccb99
ajain189 <aarushjain189@gmail.com>
2026-02-24 14:59:00 -0500

    added custom swerve drivetrain wrapper for CTRE

---
+++ RobotCode/src/main/java/com/team900/frc2026/subsystems/drive/CommandSwerveDrivetrain.java
@@ -0,0 +1,27 @@
+package com.team900.frc2026.subsystems.drive;
+
+import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+
+/**
+ * Simple container for holding CTRE swerve drivetrain creation constants. 
+ */
+public class CommandSwerveDrivetrain {
+    SwerveDrivetrainConstants driveTrainConstants;
+    SwerveModuleConstants<?, ?, ?>[] moduleConstants;
+
+    public CommandSwerveDrivetrain(
+            SwerveDrivetrainConstants driveTrainConstants,
+            SwerveModuleConstants<?, ?, ?>... modules) {
+        this.driveTrainConstants = driveTrainConstants;
+        this.moduleConstants = modules;
+    }
+
+    public SwerveDrivetrainConstants getDriveTrainConstants() {
+        return driveTrainConstants;
+    }
+
+    public SwerveModuleConstants<?, ?, ?>[] getModuleConstants() {
+        return moduleConstants;
+    }
+}
