1727558fcab6f2063f84bb0b2259de3b77308c70
ajain189 <aarushjain189@gmail.com>
2026-02-24 15:04:26 -0500

    updated build constants for drivetrain branch

--- RobotCode/src/main/java/com/team900/frc2026/BuildConstants.java
+++ RobotCode/src/main/java/com/team900/frc2026/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "1f4058afbd74c15d5b35beda048f647ea8561b36";
-  public static final String GIT_DATE = "2026-01-15 13:12:14 PST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-15 14:00:47 PST";
-  public static final long BUILD_UNIX_TIME = 1768514447518L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 46;
+  public static final String GIT_SHA = "cf3a3596a5bc80faaefa5fd590a30f7427dcefd1";
+  public static final String GIT_DATE = "2026-02-10 14:35:19 PST";
+  public static final String GIT_BRANCH = "drivetrain";
+  public static final String BUILD_DATE = "2026-02-23 15:13:10 PST";
+  public static final long BUILD_UNIX_TIME = 1771888390118L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
