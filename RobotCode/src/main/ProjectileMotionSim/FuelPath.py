5f8bc81c9af52d7af384fd4aec48bfd9a6efc9c5
anshmenghani <ansh.menghani@gmail.com>
2026-02-26 21:35:47 -0500

    added model

--- RobotCode/src/main/ProjectileMotionSim/FuelPath.py
+++ RobotCode/src/main/ProjectileMotionSim/FuelPath.py
@@ -12,11 +12,11 @@ yt = 2
 zt = 10
 
 # define robot's initial velocities and Fuel initial spin
-vx_robot = 3
-vy_robot = 3
-vz_robot = 3
+vx_robot = 6
+vy_robot = 0
+vz_robot = 6
+
 v_mag_robot = linalg.norm(array([vx_robot, vy_robot, vz_robot]))
-omega_robot = 10
 
 # define initial solution guesses
 vxi0 = 10
@@ -26,7 +26,7 @@ omegai0 = 10
 
 # define fuel object and fuel_solver object
 fuel = pp.Projectile(0.0762, 0.226796)
-fuel_solver = pp.ProjectileSolver(fuel, xt, yt, zt, vxi0, vyi0, vzi0, omegai0, vx_frame=vx_robot, vy_frame=vy_robot, vz_frame=vz_robot, omega_frame=omega_robot, fix_speed=False, fix_omega=False, lm_iters=3, sim_end_time=5, dt=0.01, clearance_func=fc.hub_clearance)
+fuel_solver = pp.ProjectileSolver(fuel, xt, yt, zt, vxi0, vyi0, vzi0, omegai0, vx_frame=vx_robot, vy_frame=vy_robot, vz_frame=vz_robot, fix_speed=True, fix_omega=Tr, lm_iters=10, sim_end_time=5, dt=0.01, clearance_func=fc.hub_clearance)
 
 # solve for valid inputs and time the solver
 start = time.perf_counter()
@@ -34,7 +34,7 @@ fuel_solver.levenberg_marquardt()
 end = time.perf_counter()
 
 # display results 
-vel_approx_tlist, vx_list, vy_list, vz_list, speedf, pos_approx_tlist, sx_list, sy_list, sz_list, omegaf = fuel.trajectory(fuel_solver.vx+vx_robot, fuel_solver.vy+vy_robot, fuel_solver.vz+vz_robot, fuel_solver.omega+omega_robot, dt=0.001, stop_on_y=yt) # smaller dt=more refined approximation
+vel_approx_tlist, vx_list, vy_list, vz_list, speedf, pos_approx_tlist, sx_list, sy_list, sz_list, omegaf = fuel.trajectory(fuel_solver.vx+vx_robot, fuel_solver.vy+vy_robot, fuel_solver.vz+vz_robot, fuel_solver.omega, dt=0.01, stop_on_y=yt) # smaller dt=more refined approximation
 
 table = [
     ["LM Time (ms)", 1000 * (end - start)],
@@ -47,7 +47,6 @@ table = [
     ["Total vx (m/s)", fuel_solver.vx + vx_robot],
     ["Total vy (m/s)", fuel_solver.vy + vy_robot],
     ["Total vz (m/s)", fuel_solver.vz + vz_robot],
-    ["Total Spin (rad/s)", fuel_solver.omega + omega_robot],
     ["Theta (deg)", rad2deg(fuel_solver.theta)],
     ["Phi (deg)", rad2deg(fuel_solver.phi)],
     ["Final X (m)", sx_list[-1]],
