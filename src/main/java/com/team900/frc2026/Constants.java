e435c9eb5dedd50507cbea58456772170460c9c5
giragon6 <emmemcdonald66@gmail.com>
2026-02-27 15:10:27 -0500

    add bridge stuff

--- src/main/java/com/team900/frc2026/Constants.java
+++ src/main/java/com/team900/frc2026/Constants.java
@@ -7,54 +7,58 @@ import java.util.Arrays;
 
 public class Constants {
 
-    public static final Mode simMode = Mode.SIM;
-    public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
-    public static final SimControllerType kSimControllerType = SimControllerType.XBOX;
+        public static final Mode simMode = Mode.SIM;
+        public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+        public static final SimControllerType kSimControllerType = SimControllerType.XBOX;
 
-    public static final boolean tuningMode = false;
+        public static final boolean tuningMode = false;
 
-    public static final double kRealDt = 0.02;
+        public static final double kRealDt = 0.02;
 
     public enum SimControllerType {
         XBOX,
         DUAL_SENSE
     }
 
-    public enum Mode {
-        /** Running on a real robot. */
-        REAL,
+        public enum SimControllerType {
+                XBOX,
+                DUAL_SENSE
+        }
 
-        /** Running a physics simulator. */
-        SIM,
+        public enum Mode {
+                /** Running on a real robot. */
+                REAL,
 
-        /** Replaying from a log file. */
-        REPLAY
-    }
+                /** Running a physics simulator. */
+                SIM,
 
-    public static boolean disableHAL = false;
+                /** Replaying from a log file. */
+                REPLAY
+        }
 
-    public static void disableHAL() {
-        disableHAL = true;
-    }
+        public static boolean disableHAL = false;
 
-    // TODO: temporary code to be changed to reflect rebuilt map
-    public static final AprilTagFieldLayout kAprilTagLayout =
-            AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
-    public static final int[] kAllowedTagIDs = {17, 18, 19, 20, 21, 22, 6, 7, 8, 9, 10, 11};
-    public static final AprilTagFieldLayout kAprilTagLayoutReefsOnly =
-            new AprilTagFieldLayout(
-                    kAprilTagLayout.getTags().stream()
-                            .filter(
-                                    tag ->
-                                            Arrays.stream(kAllowedTagIDs)
-                                                    .anyMatch(element -> element == tag.ID))
-                            .toList(),
-                    kAprilTagLayout.getFieldLength(),
-                    kAprilTagLayout.getFieldWidth());
+        public static void disableHAL() {
+                disableHAL = true;
+        }
 
-    public static final double kFieldWidthMeters = kAprilTagLayout.getFieldWidth();
-    public static final double kFieldLengthMeters = kAprilTagLayout.getFieldLength();
+        // TODO: temporary code to be changed to reflect rebuilt map
+        public static final AprilTagFieldLayout kAprilTagLayout = AprilTagFieldLayout
+                        .loadField(AprilTagFields.k2025ReefscapeWelded);
+        public static final int[] kAllowedTagIDs = { 17, 18, 19, 20, 21, 22, 6, 7, 8, 9, 10, 11 };
+        public static final AprilTagFieldLayout kAprilTagLayoutReefsOnly = new AprilTagFieldLayout(
+                        kAprilTagLayout.getTags().stream()
+                                        .filter(
+                                                        tag -> Arrays.stream(kAllowedTagIDs)
+                                                                        .anyMatch(element -> element == tag.ID))
+                                        .toList(),
+                        kAprilTagLayout.getFieldLength(),
+                        kAprilTagLayout.getFieldWidth());
 
-    public record Gains(
-            double kP, double kI, double kD, double ffkS, double ffkV, double ffkA, double ffkG) {}
+        public static final double kFieldWidthMeters = kAprilTagLayout.getFieldWidth();
+        public static final double kFieldLengthMeters = kAprilTagLayout.getFieldLength();
+
+        public record Gains(
+                        double kP, double kI, double kD, double ffkS, double ffkV, double ffkA, double ffkG) {
+        }
 }
---
+++ src/main/java/com/team900/frc2026/subsystems/coprocessor/messages/apriltag_msgs/ApriltagArrayStamped.java
@@ -0,0 +1,55 @@
+// Auto generated!! Do not modify.
+package com.team900.frc2026.subsystems.coprocessor.messages.apriltag_msgs;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.Expose;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ApriltagArrayStamped extends frc.team88.ros.messages.RosMessage {
+
+    private frc.team88.ros.messages.std_msgs.RosHeader header = new frc.team88.ros.messages.std_msgs.RosHeader();
+    private ArrayList<com.team900.frc2026.subsystems.coprocessor.messages.apriltag_msgs.Apriltag> apriltags = new ArrayList<>();
+
+    @Expose(serialize = false, deserialize = false)
+    public final java.lang.String _type = "apriltag_msgs/ApriltagArrayStamped";
+
+    public ApriltagArrayStamped() {
+
+    }
+
+    public ApriltagArrayStamped(frc.team88.ros.messages.std_msgs.RosHeader header, com.team900.frc2026.subsystems.coprocessor.messages.apriltag_msgs.Apriltag[] apriltags) {
+        this.header = header;
+        this.apriltags = new ArrayList<>(Arrays.asList(apriltags));
+    }
+
+    public ApriltagArrayStamped(JsonObject jsonObj) {
+        this.header = new frc.team88.ros.messages.std_msgs.RosHeader(jsonObj.get("header").getAsJsonObject());
+        for (JsonElement apriltags_element : jsonObj.getAsJsonArray("apriltags")) {
+            this.apriltags.add(new com.team900.frc2026.subsystems.coprocessor.messages.apriltag_msgs.Apriltag(apriltags_element.getAsJsonObject()));
+        }
+    }
+
+    public frc.team88.ros.messages.std_msgs.RosHeader getHeader() {
+        return this.header;
+    }
+    public ArrayList<com.team900.frc2026.subsystems.coprocessor.messages.apriltag_msgs.Apriltag> getApriltags() {
+        return this.apriltags;
+    }
+
+    public void setHeader(frc.team88.ros.messages.std_msgs.RosHeader header) {
+        this.header = header;
+    }
+    public void setApriltags(ArrayList<com.team900.frc2026.subsystems.coprocessor.messages.apriltag_msgs.Apriltag> apriltags) {
+        this.apriltags = apriltags;
+    }
+
+    public JsonObject toJSON() {
+        return ginst.toJsonTree(this).getAsJsonObject();
+    }
+
+    public java.lang.String toString() {
+        return ginst.toJson(this);
+    }
+}
