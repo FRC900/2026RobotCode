5fcfac9168da8d4126b4eae1f15672d3e58e245a
Vidhatu Patel <62958219+Kemchobro@users.noreply.github.com>
2026-02-27 00:57:44 -0500

    im giving up on this now until me and emme can figure out the best way to communciate with ROS

--- src/main/java/com/team900/frc2026/RobotContainer.java
+++ src/main/java/com/team900/frc2026/RobotContainer.java
@@ -6,13 +6,31 @@ package com.team900.frc2026;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+
+import java.util.function.Consumer;
+
 import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
+
+import com.team900.frc2026.subsystems.drive.DriveSubsystem;
+import com.team900.frc2026.subsystems.vision.VisionFieldPoseEstimate;
 
 public class RobotContainer {
     private static volatile RobotContainer instance;
+    private final RobotState robotState;
     public SwerveDriveSimulation driveSimulation = null;
+    public  DriveSubsystem driveSubsystem; 
+
+    private final Consumer<VisionFieldPoseEstimate> visionEstimateConsumer =
+            new Consumer<VisionFieldPoseEstimate>() {
+                @Override
+                public void accept(VisionFieldPoseEstimate estimate) {
+                    driveSubsystem.addVisionMeasurement(estimate);
+                }
+            };
 
     private RobotContainer() {
+        robotState = RobotState.getInstance(visionEstimateConsumer);
+        
         configureBindings();
     }
 
