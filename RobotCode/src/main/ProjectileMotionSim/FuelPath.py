bf9e444dd526bfdefba010c1b5bdcd47951492fb
anshmenghani <ansh.menghani@gmail.com>
2026-02-26 23:24:26 -0500

    created surface plot data gen and modeling code

--- RobotCode/src/main/ProjectileMotionSim/FuelPath.py
+++ RobotCode/src/main/ProjectileMotionSim/FuelPath.py
@@ -26,7 +26,7 @@ omegai0 = 10
 
 # define fuel object and fuel_solver object
 fuel = pp.Projectile(0.0762, 0.226796)
-fuel_solver = pp.ProjectileSolver(fuel, xt, yt, zt, vxi0, vyi0, vzi0, omegai0, vx_frame=vx_robot, vy_frame=vy_robot, vz_frame=vz_robot, fix_speed=True, fix_omega=Tr, lm_iters=10, sim_end_time=5, dt=0.01, clearance_func=fc.hub_clearance)
+fuel_solver = pp.ProjectileSolver(fuel, xt, yt, zt, vxi0, vyi0, vzi0, omegai0, vx_frame=vx_robot, vy_frame=vy_robot, vz_frame=vz_robot, fix_speed=True, fix_omega=True, lm_iters=10, sim_end_time=5, dt=0.01, clearance_func=fc.hub_clearance)
 
 # solve for valid inputs and time the solver
 start = time.perf_counter()
---
+++ RobotCode/src/main/ProjectileMotionSim/PlotShooterSurfaces.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load data
+data = np.load("clean_theta_phi_surface.npz")
+r_vals = data['r_vals']
+vf_vals = data['vf_vals']
+vl_vals = data['vl_vals']
+theta_surface = data['theta_surface']
+phi_surface = data['phi_surface']
+
+# forward (radial) velocity, distance to target --> theta
+theta_slice = theta_surface[:, :, 0] 
+R, VF = np.meshgrid(r_vals, vf_vals, indexing='ij')
+
+fig1 = plt.figure(figsize=(10,6))
+ax1 = fig1.add_subplot(111, projection='3d')
+surf1 = ax1.plot_surface(R, VF, theta_slice, cmap='viridis', edgecolor='k')
+ax1.set_xlabel('Distance to Hub r (m)')
+ax1.set_ylabel('Forward Robot Velocity vf (m/s)')
+ax1.set_zlabel('Launch Angle θ (rad)')
+ax1.set_title('Theta Surface vs Distance and Forward Velocity')
+fig1.colorbar(surf1, ax=ax1, shrink=0.5, aspect=10)
+fig1.savefig("theta_surface.png", dpi=300, bbox_inches='tight') # save plot 1
+
+# lateral (tangential) velocity, distance to target --> phi
+phi_slice = phi_surface[:, 0, :]
+R, VL = np.meshgrid(r_vals, vl_vals, indexing='ij')
+
+fig2 = plt.figure(figsize=(10,6))
+ax2 = fig2.add_subplot(111, projection='3d')
+surf2 = ax2.plot_surface(R, VL, phi_slice, cmap='plasma', edgecolor='k')
+ax2.set_xlabel('Distance to Hub r (m)')
+ax2.set_ylabel('Lateral Robot Velocity vl (m/s)')
+ax2.set_zlabel('Azimuth Angle φ (rad)')
+ax2.set_title('Phi Surface vs Distance and Lateral Velocity')
+fig2.colorbar(surf2, ax=ax2, shrink=0.5, aspect=10)
+fig2.savefig("phi_surface.png", dpi=300, bbox_inches='tight') # save plot 2
+
+plt.show()
