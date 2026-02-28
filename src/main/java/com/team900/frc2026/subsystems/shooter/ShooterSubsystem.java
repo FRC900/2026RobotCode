0ad79f98d41c16e35b44b3539d9c7371e5d78bf0
Vidhatu Patel <62958219+Kemchobro@users.noreply.github.com>
2026-02-28 10:43:46 -0500

    formatting

--- src/main/java/com/team900/frc2026/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/com/team900/frc2026/subsystems/shooter/ShooterSubsystem.java
@@ -1,11 +1,13 @@
 package com.team900.frc2026.subsystems.shooter;
 
+import com.team900.lib.subsystems.MotorInputsAutoLogged;
 import com.team900.lib.subsystems.ServoMotorSubsystemWithFollowers;
 
-public class ShooterSubsystem extends ServoMotorSubsystemWithFollowers   {
+public class ShooterSubsystem extends ServoMotorSubsystemWithFollowers<MotorInputsAutoLogged LeaderIO, MotorInputsAutoLogged>   {
 
     public ShooterSubsystem()   {
 
     }
     
 }
+ 
