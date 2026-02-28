63760d362a20f75122a5da60b3178fdf9867c8fa
anshmenghani <ansh.menghani@gmail.com>
2026-02-28 15:10:06 -0500

    path updates

--- RobotCode/src/main/ProjectileMotionSim/FuelPath.py
+++ RobotCode/src/main/ProjectileMotionSim/FuelPath.py
@@ -7,26 +7,26 @@ from tabulate import tabulate
 import time 
 
 # define targets and initial condition guesses
-xt = 12
+xt = 3
 yt = 2
 zt = 0
 
 # define robot's initial velocities and Fuel initial spin
-vx_robot = -10
+vx_robot = 0
 vy_robot = 0
-vz_robot = -10
+vz_robot = 0
 
 v_mag_robot = linalg.norm(array([vx_robot, vy_robot, vz_robot]))
 
 # define initial solution guesses
-vxi0 = 18
-vyi0 = 18
-vzi0 = 18
+vxi0 = 10
+vyi0 = 10
+vzi0 = 10
 omegai0 = 3 * 2*pi
 
 # define fuel object and fuel_solver object
 fuel = pp.Projectile(0.0762, 0.226796)
-fuel_solver = pp.ProjectileSolver(fuel, xt, yt, zt, vxi0, vyi0, vzi0, omegai0, vx_frame=vx_robot, vy_frame=vy_robot, vz_frame=vz_robot, fix_speed=True, fix_omega=True, lm_iters=5, sim_end_time=5, dt=0.01, clearance_func=fc.hub_clearance)
+fuel_solver = pp.ProjectileSolver(fuel, xt, yt, zt, vxi0, vyi0, vzi0, omegai0, vx_frame=vx_robot, vy_frame=vy_robot, vz_frame=vz_robot, fix_speed=False, fix_omega=True, lm_iters=15, sim_end_time=5, dt=0.01, clearance_func=fc.hub_clearance, theta_bounds=(0, 0), phi_bounds=(0.52, 1.04))
 
 # solve for valid inputs and time the solver
 start = time.perf_counter()
