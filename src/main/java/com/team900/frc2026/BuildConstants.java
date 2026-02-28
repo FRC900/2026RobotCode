680c451e51ecc067faef17ffbf5961542636b484
anshmenghani <ansh.menghani@gmail.com>
2026-02-28 10:32:07 -0500

    made surfaces

---
+++ src/main/java/com/team900/frc2026/BuildConstants.java
@@ -0,0 +1,19 @@
+package com.team900.frc2026;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026RobotCode";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 85;
+  public static final String GIT_SHA = "8b11e6649f3c89b0adf6b9a6ea1607b565a6aa39";
+  public static final String GIT_DATE = "2026-02-27 13:22:11 EST";
+  public static final String GIT_BRANCH = "The-Hood";
+  public static final String BUILD_DATE = "2026-02-27 13:24:05 EST";
+  public static final long BUILD_UNIX_TIME = 1772216645259L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
