800489edf8ab1d47ff53adb870772ffdb0dde75b
Rex Chen <rexchen0125@gmail.com>
2026-03-03 17:06:19 -0500

    idk anymore

--- src/main/java/com/team900/frc2026/subsystems/intake/IntakePivotSubsystem.java
+++ src/main/java/com/team900/frc2026/subsystems/intake/IntakePivotSubsystem.java
@@ -1,38 +1,59 @@
-package com.team900.frc2026.subsystems.intake;
+package com.team900.frc2026.subsystems.Intake;
 
+import com.team900.lib.subsystems.*;
+import com.team900.lib.util.Util;
+import com.team900.frc2026.subsystems.*;
 import com.team900.frc2026.RobotState;
-import com.team900.lib.subsystems.CanCoderIO;
-import com.team900.lib.subsystems.CanCoderInputsAutoLogged;
-import com.team900.lib.subsystems.MotorIO;
-import com.team900.lib.subsystems.MotorInputsAutoLogged;
-import com.team900.lib.subsystems.ServoMotorSubsystemWithCanCoder;
-import com.team900.lib.subsystems.ServoMotorSubsystemWithCanCoderConfig;
+import com.team900.lib.subsystems.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakePivotSubsystem
-        extends ServoMotorSubsystemWithCanCoder<
-                MotorInputsAutoLogged, MotorIO, CanCoderInputsAutoLogged, CanCoderIO> {
-    private final RobotState state = RobotState.getInstance();
+public class IntakePivotSubsystem extends ServoMotorSubsystemWithCanCoder<
+                MotorInputsAutoLogged, MotorIO, CanCoderInputsAutoLogged, CanCoderIO>{
+    private final RobotState state;
+    private boolean deployed = false;
 
     public IntakePivotSubsystem(
-            ServoMotorSubsystemWithCanCoderConfig config, MotorIO motorIO, CanCoderIO cancoderIO) {
-        super(
-                config,
-                new MotorInputsAutoLogged(),
-                motorIO,
-                new CanCoderInputsAutoLogged(),
-                cancoderIO);
+            final ServoMotorSubsystemWithCanCoderConfig motorConfig,
+            final MotorIO motorIO,
+            final CanCoderIO canCoderIO,
+            RobotState state) {
+        super(motorConfig, new MotorInputsAutoLogged(), motorIO, new CanCoderInputsAutoLogged(), canCoderIO);
+        this.setCurrentPosition(IntakeConstants.kIntakePivotStowPositionRadians);
+        this.positionSetpointUnits = IntakeConstants.kIntakePivotStowPositionRadians;
+        this.state = state;
+    }
 
-        setDefaultCommand(
-                motionMagicSetpointCommand(this::getPositionSetpointUnits)
-                        .withName("Intake Pivot Hold Setpoint")
-                        .ignoringDisable(true));
+    public void setAutoDefaultCommand(){
+        this.getDefaultCommand().cancel();
+        this.setDefaultCommand(motionMagicSetpointCommand(() -> 
+            IntakeConstants.kIntakePivotDeployPositionRadians)
+        .withName("Deploy Intake In Auto")
+        .ignoringDisable(true));
+    }
 
-        cancoderIO.updateFrequency(500);
+    public void setTeleopDefaultCommand(){
+        this.getDefaultCommand().cancel();
+        this.setDefaultCommand(
+            motionMagicSetpointCommand(this::getPositionSetpointUnits)
+            .withName("Intake Pivot Maintain Setpoint")
+            .ignoringDisable(true));
     }
 
     @Override
     public void periodic() {
         super.periodic();
         state.setIntakePivotRadians(inputs.unitPosition);
+        if(!deployed) {
+            deployed = inputs.unitPosition >= IntakeConstants.kIntakePivotDeployPositionRadians
+            || Util.epsilonEquals(IntakeConstants.kIntakePivotDeployPositionRadians,inputs.unitPosition,0.005);
+        }
+
+        if (this.positionSetpointUnits != IntakeConstants.kIntakePivotDeployPositionRadians) {
+            deployed = false;
+            Logger.recordOutput("IntakePivot/fullyDeployed",deployed);
+        }
     }
-}
+}   
+
--- src/main/java/com/team900/frc2026/subsystems/intake/IntakeRollerSubsystem.java
+++ src/main/java/com/team900/frc2026/subsystems/intake/IntakeRollerSubsystem.java
@@ -1,26 +1,19 @@
-package com.team900.frc2026.subsystems.intake;
+package com.team900.frc2026.subsystems.Intake;
 
+import com.team900.lib.subsystems.*;
 import com.team900.frc2026.RobotState;
-import com.team900.lib.subsystems.MotorIO;
-import com.team900.lib.subsystems.MotorInputsAutoLogged;
-import com.team900.lib.subsystems.ServoMotorSubsystem;
-import com.team900.lib.subsystems.ServoMotorSubsystemConfig;
 
-/**
- * The {@code IntakeRollerSubsystem} controls the roller mechanism of the robot's intake. It manages
- * the speed and direction of the intake rollers to collect and feed game pieces.
- */
-public class IntakeRollerSubsystem extends ServoMotorSubsystem<MotorInputsAutoLogged, MotorIO> {
-    private final RobotState state = RobotState.getInstance();
+public class IntakeRollerSubsystem extends ServoMotorSubsystem<MotorInputsAutoLogged, MotorIO>{
+    private final RobotState state;
     public MotorIO motorIO;
 
-    public IntakeRollerSubsystem(
-            final ServoMotorSubsystemConfig motorConfig, final MotorIO motorIO) {
+    public IntakeRollerSubsystem(final ServoMotorSubsystemConfig motorConfig, final MotorIO motorIO, RobotState state){
         super(motorConfig, new MotorInputsAutoLogged(), motorIO);
+        this.state = state;
         this.motorIO = motorIO;
     }
 
-    public double getPositionRotations() {
+    public double getPositionRotations(){
         return inputs.unitPosition;
     }
 
