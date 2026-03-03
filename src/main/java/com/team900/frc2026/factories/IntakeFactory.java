2943ec0e9f762fed151f546207ea1f1b603f6c96
Rex Chen <rexchen0125@gmail.com>
2026-03-03 17:59:11 -0500

    intake pivot container code

--- src/main/java/com/team900/frc2026/factories/IntakeFactory.java
+++ src/main/java/com/team900/frc2026/factories/IntakeFactory.java
@@ -1,6 +1,7 @@
 package com.team900.frc2026.factories;
 
 import com.team900.frc2026.RobotContainer;
+import com.team900.frc2026.subsystems.intake.IntakePivotConstants;
 import com.team900.frc2026.subsystems.intake.IntakeRollerConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -15,5 +16,21 @@ public class IntakeFactory {
         return container
                 .getIntakeRollerSubsystem()
                 .dutyCycleCommand(() -> IntakeRollerConstants.kIntakeDutyCycleExhaust);
+    }
+
+    public static Command deploySlapdown(RobotContainer container) {
+        return container
+                .getIntakePivotSubsystem()
+                .motionMagicSetpointCommandBlocking(
+                        () -> IntakePivotConstants.kIntakePivotDeployRadians, 
+                        IntakePivotConstants.kIntakePivotToleranceRadians);
+    }
+
+    public static Command retractSlapdown(RobotContainer container) {
+        return container
+                .getIntakePivotSubsystem()
+                .motionMagicSetpointCommandBlocking(
+                        () -> IntakePivotConstants.kIntakePivotStowRadians, 
+                        IntakePivotConstants.kIntakePivotToleranceRadians);
     }
 }
