db6bcd992138b1abf720611886b192c1f494f244
jrfranklin2009 <jrfranklin2009@gmail.com>
2026-03-04 14:23:57 -0500

    work on shootersetpoint a little

--- src/main/java/com/team900/frc2026/factories/SuperstructureFactory.java
+++ src/main/java/com/team900/frc2026/factories/SuperstructureFactory.java
@@ -1,18 +1,31 @@
 package com.team900.frc2026.factories;
 
 import com.team900.frc2026.RobotContainer;
+import com.team900.frc2026.subsystems.shooter.ShooterConstants;
+import com.team900.lib.util.ShooterSetpoint;
+
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class SuperstructureFactory {
 
-    public static Command aimAndShootAll(RobotContainer container) {
+    public static Command aimAndShoot(RobotContainer container) {
         return null;
         /*Command to shoot all balls in robot (should auto aim)
          * Aim turret
          * Aim hood
          * Maintain shooter and handoff speed
-         * Move spindexer to load to turret as needed
+         * Run spindexer
          */
 
     }
+
+    public static Command aim(RobotContainer container){
+        return null;
+        /*Command to aim at target
+         * Aim turret
+         * Aim hood
+         */
+    }
+
+
 }
--- src/main/java/com/team900/lib/util/ShooterSetpoint.java
+++ src/main/java/com/team900/lib/util/ShooterSetpoint.java
@@ -1,7 +1,12 @@
 package com.team900.lib.util;
 
 import com.team900.frc2026.RobotState;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+
 import java.util.Optional;
+
 
 public class ShooterSetpoint {
 
@@ -58,6 +63,22 @@ public class ShooterSetpoint {
         return this.isValid;
     }
 
+    private static ShooterSetpoint makeSetpoint(Rotation2d robotToTargetRotation,
+            Translation3d robotToTargetTranslation) {
+
+            //turret
+
+            //turretFeedForward
+
+            //hood
+
+            //hoodFeedForward
+
+            //shooterRPS
+
+            
+}
+
     // private static ShooterSetpoint makeSetpoint(Rotation2d robotToTargetRotation,
     //         Translation3d robotToTargetTranslation,
     //         double pitchAngleRads, double launchSpeedMetersPerSec) {
@@ -65,7 +86,7 @@ public class ShooterSetpoint {
     //     Rotation2d turretRotationRobotFrame = robotToTargetRotation
     //             .minus(robotState.getLatestFieldToRobot().getValue().getRotation());
     //     Rotation2d turretRotationTurretFrame = turretRotationRobotFrame
-    //
+    
     // .rotateBy(MathHelpers.kRotation2dPi).rotateBy(ShooterConstants.kTurretToShotCorrection);
 
     //     // hood
