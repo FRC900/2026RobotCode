4e42e5f041ce031dae8c113238392d8b03f03e14
Vidhatu Patel <62958219+Kemchobro@users.noreply.github.com>
2026-03-04 06:31:17 -0500

    spotless

--- src/main/java/com/team900/frc2026/viz/RobotViz.java
+++ src/main/java/com/team900/frc2026/viz/RobotViz.java
@@ -14,7 +14,8 @@ public class RobotViz {
     private Pose3d intakePose3d = new Pose3d();
     private Pose3d shooterPose3d = new Pose3d();
     // Offset from hood center to the hood's pivot point (in meters)
-    private static final Translation3d HOOD_PIVOT_OFFSET = new Translation3d(0.223907,0,0).minus(new Translation3d(0.148908,0,0));
+    private static final Translation3d HOOD_PIVOT_OFFSET =
+            new Translation3d(0.223907, 0, 0).minus(new Translation3d(0.148908, 0, 0));
 
     public RobotViz() {}
 
@@ -23,14 +24,18 @@ public class RobotViz {
 
             double turretRotation = state.getLatestRobotToTurret().getValue().getRadians();
 
-            shooterPose3d = new Pose3d(new Translation3d(   -0.148908,-0.152400,0.260636 + 0.09525), new Rotation3d(0, 0, turretRotation));
+            shooterPose3d =
+                    new Pose3d(
+                            new Translation3d(-0.148908, -0.152400, 0.260636 + 0.09525),
+                            new Rotation3d(0, 0, turretRotation));
             // Rotate the hood's pivot offset by the turret angle,
             // then translate the hood to that rotated pivot position
             Translation3d rotatedHoodPivot =
                     HOOD_PIVOT_OFFSET.rotateBy(new Rotation3d(0, 0, turretRotation));
             hoodPose3d =
                     new Pose3d(
-                            new Translation3d(-0.148908,-0.152400,0.342476 + 0.09525).minus(rotatedHoodPivot),
+                            new Translation3d(-0.148908, -0.152400, 0.342476 + 0.09525)
+                                    .minus(rotatedHoodPivot),
                             new Rotation3d(
                                     0,
                                     Units.rotationsToRadians(state.getHoodRotations()),
@@ -39,7 +44,7 @@ public class RobotViz {
 
         intakePose3d =
                 new Pose3d(
-                        new Translation3d(0.165101,0,0.012717 + 0.09525),
+                        new Translation3d(0.165101, 0, 0.012717 + 0.09525),
                         new Rotation3d(
                                 0, Units.rotationsToRadians(state.getIntakePivotRotations()), 0));
 
