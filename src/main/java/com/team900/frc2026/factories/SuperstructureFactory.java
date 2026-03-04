f85262a061c16ac943904aff1e99455704031237
jrfranklin2009 <jrfranklin2009@gmail.com>
2026-03-04 17:11:42 -0500

    added in aim command + implemented hood stuff a little more
    IMPORTANT:
    I changed the motorIO in the hood to a TalonFXIO cause that actually implements shooting with a feedforward. Lmk if this is an issue

--- src/main/java/com/team900/frc2026/factories/SuperstructureFactory.java
+++ src/main/java/com/team900/frc2026/factories/SuperstructureFactory.java
@@ -1,11 +1,16 @@
 package com.team900.frc2026.factories;
 
+import java.util.function.Supplier;
+
 import com.team900.frc2026.RobotContainer;
+import com.team900.lib.util.ShooterSetpoint;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 public class SuperstructureFactory {
 
-    public static Command aimAndShoot(RobotContainer container) {
+    public static Command aimAndShoot(RobotContainer container, Supplier<ShooterSetpoint> setPointSupplier) {
         return null;
         /*Command to shoot all balls in robot (should auto aim)
          * Aim turret
@@ -16,8 +21,11 @@ public class SuperstructureFactory {
 
     }
 
-    public static Command aim(RobotContainer container) {
-        return null;
+    public static Command aim(RobotContainer container, Supplier<ShooterSetpoint> setPointSupplier) {
+       // return null;
+        return new ParallelCommandGroup(
+            TurretFactory.aimTurretToPose(setPointSupplier),
+            HoodFactory.aimHoodToPose(container,setPointSupplier));        
         /*Command to aim at target
          * Aim turret
          * Aim hood
