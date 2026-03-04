bcd83b1c443f8a4d2c95c4984920b547045962c1
Aarush Jain <aarushjain189@gmail.com>
2026-03-04 00:34:04 -0500

    name change for intake roller config

--- src/main/java/com/team900/frc2026/subsystems/intake/IntakeRollerConstants.java
+++ src/main/java/com/team900/frc2026/subsystems/intake/IntakeRollerConstants.java
@@ -10,27 +10,27 @@ import com.team900.lib.subsystems.ServoMotorSubsystemConfig;
 
 public class IntakeRollerConstants {
 
-    public static ServoMotorSubsystemConfig kIntakeConfig = new ServoMotorSubsystemConfig();
+    public static ServoMotorSubsystemConfig kIntakeRollerConfig = new ServoMotorSubsystemConfig();
 
     static {
-        kIntakeConfig.name = "Intake";
-        kIntakeConfig.talonCANID = new CANDeviceId(60, new CANBus("mech"));
+        kIntakeRollerConfig.name = "Intake";
+        kIntakeRollerConfig.talonCANID = new CANDeviceId(60, new CANBus("mech"));
         // Top Roller - 16t:24t 24t:15t 30t:15t overall: 0.46875:1
         // Middle Roller - 16t:24t 24t:15t 30t:15t 24t:24t overall: 0.46875:1
         // Bottom Roller - 16t:24t 24t:15t 30t:15t 36t:36t 15t:15t overall: 0.46875:1
-        kIntakeConfig.unitToRotorRatio = 16.0 / 24.0 * 24.0 / 15.0 * 30.0 / 15.0;
+        kIntakeRollerConfig.unitToRotorRatio = 16.0 / 24.0 * 24.0 / 15.0 * 30.0 / 15.0;
 
 
-        kIntakeConfig.fxConfig = new TalonFXConfiguration();
-        kIntakeConfig.fxConfig.OpenLoopRamps = Constants.makeDefaultOpenLoopRampConfig();
-        kIntakeConfig.fxConfig.CurrentLimits.StatorCurrentLimit = 80;
-                kIntakeConfig.fxConfig.CurrentLimits.StatorCurrentLimitEnable = true;
-                        kIntakeConfig.fxConfig.CurrentLimits.SupplyCurrentLimit = 70;
-                                kIntakeConfig.fxConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
-                                        kIntakeConfig.fxConfig.CurrentLimits.SupplyCurrentLowerLimit = 40;
-                                        kIntakeConfig.fxConfig.CurrentLimits.SupplyCurrentLowerTime = 1;
-        kIntakeConfig.fxConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-        kIntakeConfig.fxConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        kIntakeRollerConfig.fxConfig = new TalonFXConfiguration();
+        kIntakeRollerConfig.fxConfig.OpenLoopRamps = Constants.makeDefaultOpenLoopRampConfig();
+        kIntakeRollerConfig.fxConfig.CurrentLimits.StatorCurrentLimit = 80;
+                kIntakeRollerConfig.fxConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+                        kIntakeRollerConfig.fxConfig.CurrentLimits.SupplyCurrentLimit = 70;
+                                kIntakeRollerConfig.fxConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
+                                        kIntakeRollerConfig.fxConfig.CurrentLimits.SupplyCurrentLowerLimit = 40;
+                                        kIntakeRollerConfig.fxConfig.CurrentLimits.SupplyCurrentLowerTime = 1;
+        kIntakeRollerConfig.fxConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        kIntakeRollerConfig.fxConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
 
 
     }
