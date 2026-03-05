9aaad29b14ff6ad3e4a8d245560fa915dc0f0360
jrfranklin2009 <jrfranklin2009@gmail.com>
2026-03-05 16:16:51 -0500

    added a lot of commands and did some placeholder stuff for shootersetpoint so it wouldn't give errors

--- src/main/java/com/team900/frc2026/factories/SuperstructureFactory.java
+++ src/main/java/com/team900/frc2026/factories/SuperstructureFactory.java
@@ -22,6 +22,15 @@ public class SuperstructureFactory {
          * Run handoff, shooter, and spindexer as appropriate**/
     }
 
+    public static Command shoot(RobotContainer container, Supplier<ShooterSetpoint> setPointSupplier) {
+        return new ParallelCommandGroup(
+                SpindexerFactory.runSpindexer(),
+                HandoffFactory.runHandoff(),
+                ShooterFactory.setShooterRPS(setPointSupplier));
+        /*Command to shoot all balls in robot (should NOT auto aim)
+         * Run handoff, shooter, and spindexer as appropriate**/
+    }
+
     public static Command aim(
             RobotContainer container, Supplier<ShooterSetpoint> setPointSupplier) {
         // return null;
@@ -32,4 +41,52 @@ public class SuperstructureFactory {
          * Aim turret
          * Aim hood */
     }
+
+    public static Command intake(RobotContainer container) {
+        return new ParallelCommandGroup(
+                IntakeFactory.runIntake(),
+                SpindexerFactory.runSpindexer());
+        /*Command to intake balls into robot
+        Assumes intake is already deployed
+         * Run intake, spindexer, and handoff as appropriate**/
+    }
+
+    public static Command deploy(RobotContainer container) {
+        return IntakeFactory.deploySlapdown();
+        /*Command to deploy intake
+         * Deploy intake**/
+    }
+
+    public static Command deploy_then_intake(RobotContainer container){
+        return new ParallelCommandGroup(
+                IntakeFactory.deploySlapdown(),
+                IntakeFactory.runIntake(),
+                SpindexerFactory.runSpindexer());
+         /*Command to deploy intake and then run intake
+         * Deploy intake
+         * Run intake, spindexer, and handoff as appropriate**/
+    }
+
+    public static Command stowIntake(RobotContainer container){
+        return IntakeFactory.retractSlapdown();
+        /*Command to stow intake
+         * Stow intake**/
+    }
+
+    public Command outtake(RobotContainer container) {
+        return IntakeFactory.exhaustIntake();
+        /*Command to outtake balls from robot
+         * Run intake in reverse**/
+    }
+
+    public Command deploy_then_outtake(RobotContainer container){
+        return new ParallelCommandGroup(
+                IntakeFactory.deploySlapdown(),
+                IntakeFactory.exhaustIntake());
+         /*Command to deploy intake and then run outtake
+         * Deploy intake
+         * Run intake in reverse**/
+    }   
+
+    
 }
