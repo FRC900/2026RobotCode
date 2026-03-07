b7ea50f0516e0b0797faa35c2f50bf26763857a0
EmmeM <emmemcdonald66@gmail.com>
2026-03-07 08:35:14 -0800

    merge

--- src/main/java/com/team900/frc2026/subsystems/vision/VisionConstants.java
+++ src/main/java/com/team900/frc2026/subsystems/vision/VisionConstants.java
@@ -8,7 +8,6 @@
 package com.team900.frc2026.subsystems.vision;
 
 import com.team900.frc2026.RobotState;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform2d;
@@ -18,37 +17,41 @@ import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
 
-  // Camera names, must match names configured on coprocessor
-  public static String camera0Name = "Turret_Camera";
-      
+    // Camera names, must match names configured on coprocessor
+    public static String camera0Name = "Turret_Camera";
 
-  // Basic filtering thresholds
-  public static double maxAmbiguity = 0.3;
-  public static double maxZError = 0.75;
+    // Basic filtering thresholds
+    public static double maxAmbiguity = 0.3;
+    public static double maxZError = 0.75;
 
-  // Standard deviation baselines, for 1 meter distance and 1 tag
-  // (Adjusted automatically based on distance and # of tags)
-  public static double linearStdDevBaseline = 0.02; // Meters
-  public static double angularStdDevBaseline = 0.06; // Radians
+    // Standard deviation baselines, for 1 meter distance and 1 tag
+    // (Adjusted automatically based on distance and # of tags)
+    public static double linearStdDevBaseline = 0.02; // Meters
+    public static double angularStdDevBaseline = 0.06; // Radians
 
-  // Standard deviation multipliers for each camera
-  // (Adjust to trust some cameras more than others)
-  public static double[] cameraStdDevFactors =
-      new double[] {
-        1.0, // Camera 0
-        1.0 // Camera 1
-      };
+    // Standard deviation multipliers for each camera
+    // (Adjust to trust some cameras more than others)
+    public static double[] cameraStdDevFactors =
+            new double[] {
+                1.0, // Camera 0
+                1.0 // Camera 1
+            };
 
-  // Multipliers to apply for MegaTag 2 observations
-  public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
-  public static double angularStdDevMegatag2Factor =
-      Double.POSITIVE_INFINITY; // No rotation data available
+    // Multipliers to apply for MegaTag 2 observations
+    public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
+    public static double angularStdDevMegatag2Factor =
+            Double.POSITIVE_INFINITY; // No rotation data available
 
-      // Camera T (Turret camera)
+    // Camera T (Turret camera)
     public static final double kCameraTPitchDegrees = 20.0;
     public static final double kCameraTPitchRads = Units.degreesToRadians(kCameraTPitchDegrees);
     public static final double kCameraTHeightOffGroundMeters = Units.inchesToMeters(8.3787);
-      public static Transform3d robotToCamera0 = new Transform3d(RobotState.getInstance().getTurretToCamera().getX(), RobotState.getInstance().getTurretToCamera().getY(), kCameraTHeightOffGroundMeters, new Rotation3d(0,0, 180));
+    public static Transform3d robotToCamera0 =
+            new Transform3d(
+                    RobotState.getInstance().getTurretToCamera().getX(),
+                    RobotState.getInstance().getTurretToCamera().getY(),
+                    kCameraTHeightOffGroundMeters,
+                    new Rotation3d(0, 0, 180));
 
     public static final String kROSTTableName = "limelight-turret";
     public static final double kRobotToCameraTForward = Units.inchesToMeters(7.8757);
