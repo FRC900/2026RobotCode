246c48b44948d7384eee39990f1d50dbbd1a4ae9
anshmenghani <ansh.menghani@gmail.com>
2026-03-07 10:25:35 -0500

    spotless

--- src/main/java/com/team900/frc2026/auto/FourPointZeroFiveMetersFromEdgeAutoToCenter.java
+++ src/main/java/com/team900/frc2026/auto/FourPointZeroFiveMetersFromEdgeAutoToCenter.java
@@ -5,8 +5,6 @@ import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import choreo.trajectory.SwerveSample;
 import com.team900.frc2026.RobotContainer;
-import com.team900.frc2026.factories.AutoFactory900;
-import com.team900.lib.util.ShooterSetpoint;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -70,7 +68,8 @@ public class FourPointZeroFiveMetersFromEdgeAutoToCenter {
         //                         AutoFactory900.resetHood(container),
         //                         AutoFactory900.alignToHub(() -> 0.0, () -> 0.0, () -> 0.0)
         //                                 .withTimeout(1.0),
-        //                         AutoFactory900.shoot(ShooterSetpoint::setpointHub).withTimeout(3.0),
+        //
+        // AutoFactory900.shoot(ShooterSetpoint::setpointHub).withTimeout(3.0),
         //                         AutoFactory900.stopShoot(ShooterSetpoint::setpointHub),
         //                         Commands.parallel(
         //                                 path.cmd(),
@@ -80,13 +79,7 @@ public class FourPointZeroFiveMetersFromEdgeAutoToCenter {
         //         );
 
         routine.active()
-                .onTrue(
-                        Commands.sequence(
-                                path.resetOdometry(),
-                                Commands.parallel(
-                                        path.cmd())
-                        )
-                );
+                .onTrue(Commands.sequence(path.resetOdometry(), Commands.parallel(path.cmd())));
 
         return routine.cmd();
     }
