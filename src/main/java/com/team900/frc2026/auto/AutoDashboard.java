16156a9a90cd3b458c8a28e01354bcd134ac0be8
anshmenghani <ansh.menghani@gmail.com>
2026-03-10 14:00:53 -0400

    created elastic dashboard and slowed roller down

--- src/main/java/com/team900/frc2026/auto/AutoDashboard.java
+++ src/main/java/com/team900/frc2026/auto/AutoDashboard.java
@@ -12,13 +12,14 @@ public class AutoDashboard {
     public AutoDashboard() {
         autoChooser.setDefaultOption("A", Autos.A());
 
-        // Add more autos here as you create them:
-        // autoChooser.addOption("My Other Auto", MyOtherAuto.getAutoCommand());
+        autoChooser.addOption("B", Autos.B());
+        autoChooser.addOption("C", Autos.C());
+        autoChooser.addOption("D", Autos.D());
+        autoChooser.addOption("E", Autos.E());
 
         SmartDashboard.putData("Auto Chooser", autoChooser);
     }
 
-    /** Call this in Robot.autonomousInit() or RobotContainer.getAutonomousCommand() */
     public Command getSelectedAuto() {
         Command selected = autoChooser.getSelected();
         return selected != null ? selected : Commands.none();
--- src/main/java/com/team900/frc2026/auto/Autos.java
+++ src/main/java/com/team900/frc2026/auto/Autos.java
@@ -12,7 +12,23 @@ import edu.wpi.first.wpilibj2.command.Commands;
 public class Autos {
     private static final RobotContainer container = RobotContainer.getInstance();
 
-    private static Command oneShot(AutoTrajectory path) {
+    private static Command simpleAuto(AutoTrajectory path) {
+        return Commands.sequence(
+                path.resetOdometry(),
+                AutoFactory900.resetHood(container),
+                // reset turret, 
+                path.cmd(),
+                Commands.parallel(
+                        AutoFactory900.alignToHub(() -> 0.0, () -> 0.0, () -> 0.0),
+                        AutoFactory900.waitSeconds(1)),
+                Commands.parallel(
+                        AutoFactory900.shoot(ShooterSetpoint::setpointHub),
+                        AutoFactory900.waitSeconds(5)),
+                AutoFactory900.stopShoot()
+        );
+    }
+
+    private static Command complexAuto(AutoTrajectory path) {
         return Commands.sequence(
                 path.resetOdometry(),
                 AutoFactory900.resetHood(container),
@@ -23,19 +39,17 @@ public class Autos {
                 Commands.parallel(
                         AutoFactory900.shoot(ShooterSetpoint::setpointHub),
                         AutoFactory900.waitSeconds(5)),
-                AutoFactory900.stopShoot());
-    }
+                AutoFactory900.stopShoot(), 
 
-    private static Command onePointFiveShots(AutoTrajectory path) {
-        return Commands.sequence(
-                oneShot(path),
-                Commands.parallel(path.cmd(), AutoFactory900.deploySlapdownAndRunIntake(container)),
-                AutoFactory900.retractSlapdown());
-    }
+                Commands.parallel(path.cmd(), 
+                        AutoFactory900.deploySlapdownAndRunIntake(container)),
+                Commands.parallel(
+                        AutoFactory900.alignToHub(() -> 0.0, () -> 0.0, () -> 0.0),
+                        AutoFactory900.waitSeconds(1)),
+                Commands.parallel(
+                        AutoFactory900.shoot(ShooterSetpoint::setpointHub),
+                        AutoFactory900.waitSeconds(5)), 
 
-    private static Command twoShots(AutoTrajectory path) {
-        return Commands.sequence(
-                onePointFiveShots(path),
                 path.cmd(),
                 Commands.parallel(
                         AutoFactory900.alignToHub(() -> 0.0, () -> 0.0, () -> 0.0),
@@ -43,42 +57,11 @@ public class Autos {
                 Commands.parallel(
                         AutoFactory900.shoot(ShooterSetpoint::setpointHub),
                         AutoFactory900.waitSeconds(5)),
-                AutoFactory900.stopShoot());
-    }
+                AutoFactory900.stopShoot(),
 
-    private static Command threeShots(AutoTrajectory path) {
-        return Commands.sequence(
-                twoShots(path),
-                Commands.parallel(path.cmd(), AutoFactory900.deploySlapdownAndRunIntake(container)),
-                AutoFactory900.retractSlapdown(),
-                Commands.parallel(
-                        AutoFactory900.alignToHub(() -> 0.0, () -> 0.0, () -> 0.0),
-                        AutoFactory900.waitSeconds(1)),
-                Commands.parallel(
-                        AutoFactory900.shoot(ShooterSetpoint::setpointHub),
-                        AutoFactory900.waitSeconds(5)),
-                AutoFactory900.stopShoot());
-    }
+                path.cmd()
+        );
 
-    public static Command C_RT_Ce() {
-        AutoFactory choreoFactory = GenericAuto.getAutoFactory();
-        AutoRoutine routine = choreoFactory.newRoutine("C_RT_Ce");
-        AutoTrajectory path = routine.trajectory("C_RT_Ce");
-
-        routine.active()
-                .onTrue(
-                        Commands.sequence(
-                                path.resetOdometry(),
-                                AutoFactory900.resetHood(container),
-                                path.cmd(),
-                                Commands.parallel(
-                                        AutoFactory900.alignToHub(() -> 0.0, () -> 0.0, () -> 0.0),
-                                        AutoFactory900.waitSeconds(1)),
-                                Commands.parallel(
-                                        AutoFactory900.shoot(ShooterSetpoint::setpointHub),
-                                        AutoFactory900.waitSeconds(5)),
-                                AutoFactory900.stopShoot()));
-        return routine.cmd();
     }
 
     public static Command A() {
@@ -88,17 +71,61 @@ public class Autos {
 
         routine.active()
                 .onTrue(
-                        Commands.sequence(
-                                path.resetOdometry(),
-                                AutoFactory900.resetHood(container),
-                                path.cmd(),
-                                Commands.parallel(
-                                        AutoFactory900.alignToHub(() -> 0.0, () -> 0.0, () -> 0.0),
-                                        AutoFactory900.waitSeconds(1)),
-                                Commands.parallel(
-                                        AutoFactory900.shoot(ShooterSetpoint::setpointHub),
-                                        AutoFactory900.waitSeconds(5)),
-                                AutoFactory900.stopShoot()));
+                        simpleAuto(path)
+                );
+
+        return routine.cmd();
+    }
+
+    public static Command B() {
+        AutoFactory choreoFactory = GenericAuto.getAutoFactory();
+        AutoRoutine routine = choreoFactory.newRoutine("B");
+        AutoTrajectory path = routine.trajectory("B");
+
+        routine.active()
+                .onTrue(
+                        simpleAuto(path)
+                );
+
+        return routine.cmd();
+    }
+
+    public static Command C() {
+        AutoFactory choreoFactory = GenericAuto.getAutoFactory();
+        AutoRoutine routine = choreoFactory.newRoutine("C");
+        AutoTrajectory path = routine.trajectory("C");
+
+        routine.active()
+                .onTrue(
+                        simpleAuto(path)
+                );
+
+        return routine.cmd();
+    }
+
+    public static Command D() {
+        AutoFactory choreoFactory = GenericAuto.getAutoFactory();
+        AutoRoutine routine = choreoFactory.newRoutine("D");
+        AutoTrajectory path = routine.trajectory("D");
+
+        routine.active()
+                .onTrue(
+                        simpleAuto(path)
+                );
+
+        return routine.cmd();
+    }
+
+    public static Command E() {
+        AutoFactory choreoFactory = GenericAuto.getAutoFactory();
+        AutoRoutine routine = choreoFactory.newRoutine("E");
+        AutoTrajectory path = routine.trajectory("E");
+
+        routine.active()
+                .onTrue(
+                        simpleAuto(path)
+                );
+
         return routine.cmd();
     }
 }
