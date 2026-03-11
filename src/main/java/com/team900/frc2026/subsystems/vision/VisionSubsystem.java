dbba3be5dd21266cb349c185e8b4c216d621a8fe
Aarush Jain <aarushjain189@gmail.com>
2026-03-11 16:28:59 -0400

    error fix

--- src/main/java/com/team900/frc2026/subsystems/vision/VisionSubsystem.java
+++ src/main/java/com/team900/frc2026/subsystems/vision/VisionSubsystem.java
@@ -33,6 +33,9 @@ public class VisionSubsystem extends SubsystemBase {
     private final VisionIOInputsAutoLogged[] inputs;
     private final Alert[] disconnectedAlerts;
 
+    @Getter private Rotation2d rotation2dToHubTx = new Rotation2d();
+    @Getter private Rotation2d rotation2dToHubTy = new Rotation2d();
+
     public VisionSubsystem(VisionConsumer consumer, VisionIO... io) {
         this.consumer = consumer;
         this.io = io;
