47ac9874977f6cfc27b43cae1e03e9c7e705d1ea
Rex Chen <rexchen0125@gmail.com>
2026-03-12 16:48:54 -0400

    Aiming command changes
    someone plz look at it not trying to cook the robot

--- src/main/java/com/team900/frc2026/factories/ShootingFactory.java
+++ src/main/java/com/team900/frc2026/factories/ShootingFactory.java
@@ -4,14 +4,48 @@
 
 package com.team900.frc2026.factories;
 
+import com.team900.frc2026.RobotContainer;
+import com.team900.frc2026.RobotState;
+import com.team900.lib.util.ShooterSetpoint;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import java.util.function.Supplier;
 
 public class ShootingFactory {
 
     /* Commands for shooting */
 
-    public static Command SpinBoth() {
-
-        return null;
+    public static Command shoot(
+            Supplier<ShooterSetpoint> setPointSupplier, RobotContainer container) {
+        return (new ParallelCommandGroup(
+                                ShooterFactory.setShooterRPS(setPointSupplier, container),
+                                SuperstructureFactory.aim(setPointSupplier, container))
+                        .until(
+                                () ->
+                                        MathUtil.isNear(
+                                                        setPointSupplier.get().getShooterRPS(),
+                                                        container
+                                                                .getShooterSubsystem()
+                                                                .getCurrentVelocity(),
+                                                        1)
+                                                && MathUtil.isNear(
+                                                        setPointSupplier.get().getHoodRadians(),
+                                                        container
+                                                                .getHoodSubsystem()
+                                                                .getCurrentPosition(),
+                                                        1)
+                                                && MathUtil.isNear(
+                                                        0,
+                                                        RobotState.getInstance()
+                                                                .getLatestRotationRobotToHub()
+                                                                .getDegrees(),
+                                                        3)))
+                .andThen(
+                        new ParallelCommandGroup(
+                                        IntakeFactory.runIntake(container),
+                                        HandoffFactory.runHandoff(container),
+                                        SpindexerFactory.runSpindexer(container))
+                                .onlyWhile(container.getDriveCommand()::isNearTarget));
     }
 }
