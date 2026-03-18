697a4354315880f82b35d944830f7fff9366671c
anshmenghani <ansh.menghani@gmail.com>
2026-03-18 17:03:15 -0400

    Update TrajectorySurface.py

--- ProjectileMotionSim/TrajectorySurfaces/TrajectorySurface.py
+++ ProjectileMotionSim/TrajectorySurfaces/TrajectorySurface.py
@@ -106,8 +106,8 @@ def gen_surface(speed, spin, name=""):
                     fix_omega=True,
                     phi_bounds=(0.785398, 1.309),
                     theta_bounds=(0, 5.93411946),
-                    lm_iters=5,
-                    sim_end_time=5,
+                    lm_iters=20,
+                    sim_end_time=10,
                     dt=0.01,
                     clearance_func=fc.hub_clearance
                 )
