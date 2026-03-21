d286030277dd0e6165f55814541dec15dd66c264
anshmenghani <ansh.menghani@gmail.com>
2026-03-21 12:55:24 -0400

    Update ShooterSetpoint.java

--- src/main/java/com/team900/lib/util/ShooterSetpoint.java
+++ src/main/java/com/team900/lib/util/ShooterSetpoint.java
@@ -52,10 +52,10 @@ public class ShooterSetpoint {
                                 "src/main/deploy/trajectories/30_RPS/theta_model.json");
                 phiShootingModelFar =
                         PolynomialModel.load(
-                                "/home/lvuser/deploy/trajectories/34_RPS/phi_model.json");
+                                "src/main/deploy/trajectories/34_RPS/phi_model.json");
                 thetaShootingModelFar =
                         PolynomialModel.load(
-                                "/home/lvuser/deploy/trajectories/34_RPS/theta_model.json");
+                                "src/main/deploy/trajectories/34_RPS/theta_model.json");
             }
 
         } catch (IOException e) {
